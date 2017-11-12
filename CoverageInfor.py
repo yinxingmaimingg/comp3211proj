9cf2f18979ed7ac90a22bb3f74a481067808314b
yinxingmaimingg <yinxingmaimingg@hotmail.com>
2017-11-12 21:55:23 +0800

    init

---
+++ CoverageInfor.py
@@ -0,0 +1,11 @@
+class CoverageInfor:
+
+	def __init__(self):
+		# each element is a dictionary, in the format of {"fileName":string, "lno": int} 
+		inforList = [] 
+
+	def readGcov:
+		println("readGcov")
+		for i in range(100):
+			inforList.append({"fileName":"a.cpp", "lno":i+1})
+
