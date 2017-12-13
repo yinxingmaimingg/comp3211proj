56e25704fa81e28e37ac48c3459c332d4111c825
wkkmike <michaelwang404@hotmail.com>
2017-12-13 11:07:41 +0800

    debug tarantula

--- program/Tarantula.py
+++ program/Tarantula.py
@@ -13,11 +13,11 @@ def rankBySuspiciousness(statementStatistic):
     suspiciousness = [];
     i = 0;
     for statement in statementStatistic:
-        i += 1;
         if statement[0] != -1:
             suspiciousness.append([i,getSuspiciousnessForEachStatement(statement)]);
-    sorted(suspiciousness, key = itemgetter(1),reverse = True)
-    return suspiciousness;
+        i += 1;
+    sus = sorted(suspiciousness, key = itemgetter(1),reverse = True)
+    return sus;
 
 def deleteUnuse(origin, out):
     f = open(origin,'r');
@@ -48,4 +48,9 @@ def preProcess(doc):
         element4 = int(lineElement[4][:-1]);
         statistic[index] = [element1, element2, element3, element4];
     return statistic, max;
-deleteUnuse("./SourceStatisticData/test.txt");
+
+
+
+statistic, max = preProcess('./test/2.txt');
+rank = rankBySuspiciousness(statistic);
+print(rank);
